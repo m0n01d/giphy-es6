aed95f52666bdb6de1adccbb5ec52d57bdd80923
Dwight <dwightdoane@me.com>
2016-10-21 08:35:31 -0400

    giphy key and topics

---
+++ app/main.js
@@ -0,0 +1,13 @@
+const key = `&api_key=dc6zaTOxFJmzC`;
+const giphy = `http://api.giphy.com/v1/gifs/search?q=`
+
+const topics = [
+  'cats',
+  'dogs',
+  'penguins',
+  'giraffes',
+  'elephants',
+  'dragons',
+  'hedgehog',
+  'frog',
+];
