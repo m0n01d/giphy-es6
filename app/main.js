a3748b2a3690b20af63a926509ad40f3d4d4ab86
Dwight <dwightdoane@me.com>
2016-10-21 20:53:26 -0400

    finished

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
 const key = `&api_key=dc6zaTOxFJmzC`;
 const giphy = `http://api.giphy.com/v1/gifs/search?q=`
 
-const topics = [
+let topics = [
   'cats',
   'dogs',
-  'penguins',
+  'penguins',i
   'giraffes',
   'elephants',
   'dragons',
@@ -27,11 +27,45 @@ class ButtonGroup {
   }
 }
 
+class AddTopic {
+  constructor({onEnter}) {
+    this.newTopic = document.getElementById('new-topic');
+    this.newTopic.addEventListener('keyup', (e) => {
+      if (e.which === 13) {
+        onEnter(e.target.value);
+        e.target.value = '';
+      }
+    }, false);
+  }
+}
+
+class GifSet {
+  constructor({gifs, renderGifs}) {
+    this.$gifs = document.getElementById('gifs');
+  }
+  renderGifs(gifs) {
+    this.$gifs.innerHTML = '';
+    this.$gifs.innerHTML = gifs.map(gif => (
+      `<div class="gif">
+        <img class="gif__img" src="${gif.images.fixed_width.webp}" />
+        <p>Raing: ${gif.rating}</p>
+      </div>`
+    ))
+  }
+}
+
 class App {
   constructor() {
+    this.addTopic = new AddTopic({
+      onEnter: this.addBtn.bind(this),
+    });
     this.btnGrp = new ButtonGroup({
       handleClick: this.btnClick.bind(this),
     });
+    this.gifSet = new GifSet({
+      gifs: [],
+      renderGifs: this.renderGifs
+    })
   }
   btnClick(topic) {
     fetch(`${giphy}${topic}${key}`)
@@ -40,7 +74,10 @@ class App {
         this.gifSet.renderGifs(res.data)
       });
   }
-
+  addBtn(topic) {
+    topics = [topic, ...topics];
+    this.btnGrp.renderAll();
+  }
 }
 
 new App;
