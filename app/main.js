e20a33306a13714f0db142a10b16874c5553cccb
Dwight <dwightdoane@me.com>
2016-10-21 20:48:18 -0400

    adds button group

--- app/main.js
+++ app/main.js
@@ -12,9 +12,33 @@ const topics = [
   'frog',
 ];
 
+class ButtonGroup {
+  constructor({handleClick}) {
+    this.$buttons = document.getElementById('buttons');
+    this.$buttons.addEventListener('click', e => handleClick(e.target.dataset.topic), false);
+    this.renderAll();
+  }
+  renderOne(topic) {
+    return `<button data-topic="${topic.toLowerCase()}">${topic}</button>`
+  }
+  renderAll() {
+    this.$buttons.innerHTML = '';
+    this.$buttons.innerHTML = topics.map(this.renderOne).join('')
+  }
+}
+
 class App {
   constructor() {
-
+    this.btnGrp = new ButtonGroup({
+      handleClick: this.btnClick.bind(this),
+    });
+  }
+  btnClick(topic) {
+    fetch(`${giphy}${topic}${key}`)
+      .then(res => res.json())
+      .then(res => {
+        this.gifSet.renderGifs(res.data)
+      });
   }
 
 }
